929da2e4d55f27a058c52988b27fa267f7ce7b7e
JC555 <572670014@qq.com>
2019-09-10 11:19:15 +0800

    模板类实现SeqList（模板类的实现应该放到一个.h文件中）

---
+++ seqlist.h
@@ -0,0 +1,33 @@
+#pragma once
+
+
+template <class T>
+class SeqList
+{
+	T * m_data;
+	size_t m_size;
+	size_t m_capacity;
+
+	void checkCapacity();//À©ÈÝº¯Êý
+public :
+	SeqList(size_t capacity = 10) :
+		m_data(new T[capacity]),
+		m_size(0),
+		m_capacity(capacity)
+	{
+	}
+
+	T & operator [](int i)
+	{
+		return m_data[i];                        
+	}
+   
+
+
+	void push_back(const T &src);
+	void pop_back();
+
+	int size();
+
+	~SeqList();
+};
